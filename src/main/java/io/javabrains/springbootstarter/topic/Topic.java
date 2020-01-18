06e8b87131128cee6bf3f1a74b72940c637512e1
Oleksiy Blavat <blavat.alex@gmail.com>
2020-01-17 20:11:33 -0500

    Topics controller and Topics Class

--- src/main/java/io/javabrains/springbootstarter/topic/Topic.java
+++ src/main/java/io/javabrains/springbootstarter/topic/Topic.java
@@ -8,6 +8,12 @@ public class Topic {
     private String name;
     private String description;
 
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     public String getId() {
         return id;
     }
