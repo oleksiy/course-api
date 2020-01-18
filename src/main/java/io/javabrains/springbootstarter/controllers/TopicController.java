b8d750e1485c2061f9c8c6ae993157331ef56abe
Oleksiy Blavat <blavat.alex@gmail.com>
2020-01-18 00:34:10 -0500

    Updating a topic - Request Mapping and Service logic added

--- src/main/java/io/javabrains/springbootstarter/controllers/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/controllers/TopicController.java
@@ -27,8 +27,13 @@ public class TopicController {
         return topicService.getTopic(id);
     }
 
-    @RequestMapping(method=RequestMethod.POST, value="/topics")
+    @RequestMapping(method = RequestMethod.POST, value="/topics")
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
     }
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -40,4 +40,14 @@ public class TopicService {
         //TODO: Implement a constraint to not add duplicate topics
         topics.add(topic);
     }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i = 0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equalsIgnoreCase(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
 }
