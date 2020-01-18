1179fb1cf6b66f2124b11e304f74628bffea77be
Oleksiy Blavat <blavat.alex@gmail.com>
2020-01-17 22:38:44 -0500

    Added a getById Topic mapping. It's called getTopic :computer:

--- src/main/java/io/javabrains/springbootstarter/controllers/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/controllers/TopicController.java
@@ -3,10 +3,10 @@ package io.javabrains.springbootstarter.controllers;
 import io.javabrains.springbootstarter.topic.Topic;
 import io.javabrains.springbootstarter.topic.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,5 +18,10 @@ public class TopicController {
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("topics/{topicKey}") //or just call it the same name and it will map the path variable automatically
+    public Topic getTopic(@PathVariable("topicKey") String id) {
+        return topicService.getTopic(id);
     }
 }
